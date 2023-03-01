81504456b186c3b619182a6136057dbc4768f94a
wulizhaomin <55172641+MJscofield@users.noreply.github.com>
2023-03-01 16:47:12 +0800

    Update install.sh
    Add proxy

--- install.sh
+++ install.sh
@@ -10,16 +10,16 @@
 ### 
 
 install_deeplx(){
-    last_version=$(curl -Ls "https://api.github.com/repos/OwO-Network/DeepLX/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    last_version=$(curl -Ls "https://ghproxy.com/https://api.github.com/repos/OwO-Network/DeepLX/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
     if [[ ! -n "$last_version" ]]; then
         echo -e "${red}Failed to detect DeepLX version, probably due to exceeding Github API limitations.${plain}"
         exit 1
     fi
     echo -e "DeepLX latest version: ${last_version}, Start install..."
-    wget -q -N --no-check-certificate -O /usr/bin/deeplx https://github.com/OwO-Network/DeepLX/releases/download/${last_version}/deeplx_linux_amd64
+    wget -q -N --no-check-certificate -O /usr/bin/deeplx https://ghproxy.com/https://github.com/OwO-Network/DeepLX/releases/download/${last_version}/deeplx_linux_amd64
 
     chmod +x /usr/bin/deeplx
-    wget -q -N --no-check-certificate -O /etc/systemd/system/deeplx.service https://raw.githubusercontent.com/OwO-Network/DeepLX/main/deeplx.service
+    wget -q -N --no-check-certificate -O /etc/systemd/system/deeplx.service https://ghproxy.com/https://raw.githubusercontent.com/OwO-Network/DeepLX/main/deeplx.service
     systemctl daemon-reload
     systemctl enable deeplx
     systemctl start deeplx
